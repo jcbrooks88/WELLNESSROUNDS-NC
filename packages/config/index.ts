4c998a0abc7861aa3e3a7f0eafa37459ce7b0b31
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-04-15 11:03:04 -0400

    packages update

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,13 +1,18 @@
-// packages/config/index.ts
+// ✅ Loads & exports config
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Dynamically find the .env file path (in same dir as this file)
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-export const config = {
+interface AppConfig {
+    mongoURI: string;
+    jwtSecret: string;
+    nodeEnv: string;
+  }
+
+export const AppConfig = {
   mongoURI: process.env.MONGODB_URI || "",
   jwtSecret: process.env.JWT_SECRET || "",
   nodeEnv: process.env.NODE_ENV || "development",
