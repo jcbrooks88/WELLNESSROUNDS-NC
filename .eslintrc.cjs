19b3200ae2a5057273cc9b2b71838a3fae4fb0b5
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-04-15 09:17:03 -0400

    folder structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "react"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended"
+    ],
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    env: {
+      browser: true,
+      node: true,
+      es2022: true
+    }
+  };
+  
