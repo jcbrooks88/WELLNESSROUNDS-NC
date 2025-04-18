b87f47767e77ed11d8bfe7330decba629db31676
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-04-18 08:54:40 -0400

    card.tsx

---
+++ apps/client/src/components/UI/Card.tsx
@@ -0,0 +1,13 @@
+type CardProps = {
+    children: React.ReactNode;
+    className?: string;
+  };
+  
+  export function Card({ children, className = '' }: CardProps) {
+    return (
+      <div className={`bg-white rounded-2xl shadow p-4 ${className}`}>
+        {children}
+      </div>
+    );
+  }
+  
