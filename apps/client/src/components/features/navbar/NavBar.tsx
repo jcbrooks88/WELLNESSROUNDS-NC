8b2dd89740fe2190d75d2f5d836641aaf2ace4cd
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-04-18 15:16:28 -0400

    NavBar.css & App.css

--- apps/client/src/components/features/navbar/NavBar.tsx
+++ apps/client/src/components/features/navbar/NavBar.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthContext";
 import { LogoutButton } from "../login/LogoutButton";
+import './NavBar.css';
+
 
 const NavBar: React.FC = () => {
   const { user } = useContext(AuthContext)!;
